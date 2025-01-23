3e20da76e56f5a37b3d117d4663855e3aec010b1
rowancape <niklasmk.pihlajaniemi@gmail.com>
2025-01-23 20:34:40 +0200

    This code is the most horrendous thing I've ever written but I think it might do what I want it to now

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys
+from itertools import product
 
 # fmt: off
 field = [[0, 0, 0, 1], 
@@ -58,9 +58,9 @@ def mainLoop():
                     )
                     break
 
-                if point != 1:
+                if point == 0:
                     possible_start_points[obj_index].append(
-                        [point_index + 1, row_index + 1, layer_index + 1]
+                        [layer_index, row_index, point_index]
                     )
 
 
@@ -76,6 +76,31 @@ for obj_index, object in enumerate(objects):
     obj_points = len(object[0][0])
     mainLoop()
 
-for single_obj_start_points in possible_start_points:
-    print(single_obj_start_points)
+for obj1_sp, obj2_sp, obj3_sp, obj4_sp in product(*possible_start_points):
+    obj_sps = [obj1_sp, obj2_sp, obj3_sp, obj4_sp]
 
+    for obj_index, object in enumerate(objects):
+        for li, layer in enumerate(object):
+            for ri, row in enumerate(layer):
+                for pi, point in enumerate(row):
+                    calc_field[obj_sps[obj_index][0]+li][obj_sps[obj_index][1]+ri][obj_sps[obj_index][2]+pi] += point
+
+    num_layers = len(calc_field)
+    num_rows = len(calc_field[0])
+    num_cols = len(calc_field[0][0])
+    wasSuccess = 0
+
+    for layer_idx, row_idx, col_idx in product(range(num_layers), range(num_rows), range(num_cols)):
+        point = calc_field[layer_idx][row_idx][col_idx]
+
+        if point != 1:
+            calc_field = [[[0, 0, 0, 1], [1, 0, 0, 0], [0, 0, 0, 0]], [[0, 0, 0, 1], [1, 0, 0, 0], [0, 0, 0, 0]]]
+            wasSuccess += 1
+            break
+    
+    if wasSuccess == 0:
+        print("SUCCESS!")
+        for layer in calc_field:
+            for row in layer:
+                print(row)
+    
